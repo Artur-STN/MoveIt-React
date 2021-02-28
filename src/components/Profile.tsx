af33670a778314ab357743ad1bdcb58da1507b19
Artur Santos <artur_teixeira10@hotmail.com>
2021-02-28 16:12:38 -0300

    Pastas e arquivos

---
+++ src/components/Profile.tsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+
+    const { level } = useContext(ChallengesContext);
+
+
+    return (
+
+        <div className={styles.profileContainer}>
+
+            <img src="https://github.com/artur-stn.png" alt="Imagem Perfil"/>
+
+            <div>
+
+                <strong>Artur Santos</strong>
+
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+
+                    Level { level }
+                
+                </p>
+
+            </div>
+    
+        </div>
+
+    )
+
+}
